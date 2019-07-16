292abeb35dd81d675662291a8433d586ff8d0779
erozonachi <erozonachi.james@gmail.com>
2019-07-16 14:32:57 +0100

    Add insert function to the cars model

--- models/carsModel.js
+++ models/carsModel.js
@@ -1,3 +1,9 @@
 const db = require('../data/dbConfig');
 
-module.exports = {}
+module.exports = {
+  insert: function (newCar) {
+    return db('cars')
+    .insert(newCar)
+    .then(([id]) => this.get(id));
+  },
+}
