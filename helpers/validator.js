93718eadaa601b648e3f9f9acabc7e3c871f1b8f
erozonachi <erozonachi.james@gmail.com>
2019-07-16 14:57:41 +0100

    Add validate-id function to the validator module

--- helpers/validator.js
+++ helpers/validator.js
@@ -1,3 +1,18 @@
 const carsModel = require('../models/carsModel');
 
-module.exports = {};
+module.exports = {
+  validateId: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      const car = await carsModel.get(id);
+      if(car[0] && car[0].make) {
+        req.car = car[0];
+        return next();
+      }
+      res.status(400).json({ message: 'Invalid car id' });
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
+};
