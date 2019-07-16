048eac62c2f6b8e84d0dbe7db13c1ca375fd559a
erozonachi <erozonachi.james@gmail.com>
2019-07-16 15:34:27 +0100

    Add validate-car function to the validator module

--- helpers/validator.js
+++ helpers/validator.js
@@ -15,4 +15,37 @@ module.exports = {
       res.status(500).json({ error: 'server error' });
     }
   },
+
+  validateCar: async (req, res, next) => {
+    try {
+      const { make, model, vin, mileage, transmission_type, status } = req.body;
+
+      if(!make || make.trim() === '') {
+        return res.status(400).json({ message: 'Missing required make field'});
+      }
+      if(!model || model.trim() === '') {
+        return res.status(400).json({ message: 'Missing required model field'});
+      }
+      if(!vin || vin.trim() === '') {
+        return res.status(400).json({ message: 'Missing required vin field'});
+      }
+      if(!mileage || Number.isNaN(mileage)) {
+        return res.status(400).json({ message: 'Missing or invalid required mileage field'});
+      }
+      if(transmission_type) {
+        if(transmission_type.trim() === '') {
+          return res.status(400).json({ message: 'Missing required transmission type field'});
+        }
+      }
+      if(status) {
+        if(status.trim() === '') {
+          return res.status(400).json({ message: 'Missing required status field'});
+        }
+      }
+
+      next();
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
 };
