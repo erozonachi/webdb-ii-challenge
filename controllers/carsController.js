a58ecb3e3c587f0aef9983043b6114ba2611060a
erozonachi <erozonachi.james@gmail.com>
2019-07-16 15:41:50 +0100

    Add create function to the cars controller

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -1,3 +1,12 @@
 const carsModel = require('../models/carsModel');
 
-module.exports = {};
+module.exports = {
+  create: async (req, res) => {
+    try {
+      const result = await carsModel.insert(req.body);
+      res.status(201).json(result[0]);
+    } catch(error) {
+      res.status(500).json({ error: 'server error' });
+    }
+  },
+};
